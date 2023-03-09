a6a6a942d64d666819fd86e4d6eff9d7b64a362e
Sergey Kiskin <sector1200@gmail.com>
2023-03-09 18:17:55 +0500

    session

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
-const { MetadataAuthService } = require('ydb-sdk');
+const { Driver, MetadataAuthService } = require('ydb-sdk');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const authService = new MetadataAuthService();
 
 console.log('test log');
+
+const endpoint = "grpcs://ydb.serverless.yandexcloud.net:2135";
+const database = "/ru-central1/b1g6i0mgtrt63nhpbko3/etnc9qvcjdtua52m6nfn"
+const authService = new MetadataAuthService();
+
+const driver = new Driver({ endpoint, database, authService });
+const timeout = 10000;
+if (!(await driver.ready(timeout))) {
+  logger.fatal(`Driver has not become ready in ${timeout}ms!`);
+  process.exit(1);
+}
+
+const session = await driver.tableClient.withSession(async(session) => session);
 
 app.get("/hello", (req, res) => {
     var ip = req.headers['x-forwarded-for']
     console.log(`Request from ${ip}`);
     console.log(`authService ${authService}`);
-    console.log(`authService stringify ${JSON.stringify(obj)}`);
+    console.log(`authService stringify ${JSON.stringify(authService)}`);
+    console.log(`session ${session.sessionId}`);
+
     return res.send("Hello!");
 });
 
